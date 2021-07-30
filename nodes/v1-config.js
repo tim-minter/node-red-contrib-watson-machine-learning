44ebb372ec8ff8a042bcba5f268207e805f4a9bc
Tim Minter <timminter@yahoo.co.uk>
2021-07-30 10:56:01 +0100

    Replaced instanceid with spaceid

--- nodes/v1-config.js
+++ nodes/v1-config.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2018 IBM Corp.
+ * Copyright 2021 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the 'License');
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@
      RED.nodes.createNode(this, config);
 
      this.host = config.host;
-     this.instanceid = config.instanceid;
+     this.spaceid = config.spaceid;
 
      var credentials = this.credentials;
 
